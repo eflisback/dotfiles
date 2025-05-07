c6833aecb77b78e015eb8e94371e48a355ac7076
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-05-07 15:50:18 +0200

    Test again

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+killall -q polybar
+
+if [ -z "$(pgrep -x polybar)" ]; then
+    BAR="main"
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
+        MONITOR=$m polybar --reload $BAR &
+        sleep 1
+    done
+else
+    polybar-msg cmd restart
+fi
