94c3af9e1b05b4204b0a2a4a16e3650b4b71e066
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-05-08 19:28:46 +0200

    Wallpapers and aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -2,3 +2,7 @@ alias g='git'
 alias ls='eza --long --all --icons=always --color=always'
 alias lst='eza --tree --level=2 --icons=always --color=always'
 alias rm='rm -r'
+
+alias install='yay -S'
+alias uninstall='yay -Rscn'
+alias update='yay --noconfirm'
